6debeb7dcc48770a439a21db5b20ef05ab1dd085
Simon Berger <simon@siku2.io>
2019-12-29 18:23:44 +0100

    crontask cd to app

--- docker/crontask.sh
+++ docker/crontask.sh
@@ -3,5 +3,7 @@
 # Optimize shell for safety.
 set -o errexit -o noclobber -o nounset -o pipefail
 
+cd /usr/src/app
+
 timeout 300 \
     python -m airthingswave-mqtt config.yaml > /proc/1/fd/1 2>&1
